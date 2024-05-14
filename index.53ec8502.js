53e0ad47fe1694c8d6db973229ba79c4bf59fa75
katya-sn <kattya.sagun@gmail.com>
2024-05-14 11:30:52 +0300

    make build

---
+++ index.53ec8502.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in n.append(r),t){var l=document.createElement("li");l.textContent=a,r.appendChild(l),Object.keys(t[a]).length&&e(l,t[a])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.53ec8502.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const list = document.createElement("ul");
+    element.append(list);
+    for(const key in data){
+        const item = document.createElement("li");
+        item.textContent = key;
+        list.appendChild(item);
+        if (Object.keys(data[key]).length) createTree(item, data[key]);
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
